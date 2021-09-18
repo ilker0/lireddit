f1e1de7dc53bd954c7f40ca23235fd4b9cdb35be
ilker0 <ilker46580@gmail.com>
2021-09-18 20:18:19 +0300

    auth added

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { MikroORM } from "@mikro-orm/core";
 import path from "path";
 import { __prod__ } from "./contants";
 import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 
 export default {
   dbName: "lireddit",
   type: "postgresql",
   password: "root",
   debug: !__prod__,
-  entities: [Post],
+  entities: [Post, User],
   migrations: {
     path: path.join(__dirname, "../src/migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
---
+++ server/src/resolvers/user.ts
@@ -0,0 +1,93 @@
+import { User } from "../entities/User";
+import { MyContext } from "src/types";
+import {
+  Resolver,
+  Mutation,
+  Arg,
+  InputType,
+  Field,
+  Ctx,
+  ObjectType,
+} from "type-graphql";
+import argon2 from "argon2";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field(() => String)
+  username: string;
+
+  @Field(() => String)
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+  @Mutation(() => User)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ) {
+    const { username, password } = options;
+    const hashedPassword = await argon2.hash(password);
+    const user = em.create(User, { username, password: hashedPassword });
+
+    await em.persistAndFlush(user);
+
+    return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const { username, password } = options;
+    const user = await em.findOne(User, { username });
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "could not find a username",
+          },
+        ],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, password);
+
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "incorrect password",
+          },
+        ],
+      };
+    }
+
+    return {
+      user,
+    };
+  }
+}
